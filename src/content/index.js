d360ca54ed277d43111cb717a889a4591550fffe
Ciao Chung <ciao0958@gmail.com>
2018-06-23 21:38:46 +0800

    update popup

---
+++ src/content/index.js
@@ -0,0 +1,1 @@
+document.getElementById('lst-ib').value = 'foo bar ...'
--- src/popup/root.vue
+++ src/popup/root.vue
@@ -1,14 +1,45 @@
 <template>
-  <div>
-    <h1>popup</h1>
+  <div popup class="container">
+    <input type="text" class="form-control" v-model="keywords">
+    <button class="btn btn-info" @click="save">儲存</button>
   </div>
 </template>
 
 <script>
 import bootstrap from 'bootstrap/dist/js/bootstrap.min.js'
-export default {}
+export default {
+  data() {
+    return {
+      keywords: null
+    }
+  },
+  methods: {
+    search() {
+      chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, {
+          formHelper: {
+            action: 'search',
+          },
+        })
+      })
+    },
+    save() {
+      alert('save')
+      chrome.storage.sync.set({
+        keywords: this.keywords,
+      })
+    }
+  },
+}
 </script>
 
 <style src="bootstrap/dist/css/bootstrap.min.css"></style>
 <style src="font-awesome/css/font-awesome.min.css"></style>
-<style lang="sass" scoped></style>
+<style lang="sass" scoped>
+div[popup]
+  width: 300px
+  height: 200px
+  padding: 15px
+  &>*
+    margin: 10px 0
+</style>
